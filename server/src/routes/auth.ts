23f57472cea2df51cc3bf3ea115a9d85528fd524
M-Weslley <michel_weslley@hotmail.com>
2026-04-25 11:26:32 -0300

    fix: adicionar __dirname para ESM em todas rotas

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,6 +3,10 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const USERS_FILE = path.resolve(__dirname, '../../../data/users.json');
 
